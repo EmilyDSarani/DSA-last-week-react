eb1fca73758eee99640cc35f1e2069b5c8f80ecd
EmilyDSarani <emily.sarani@gmail.com>
2022-01-04 14:59:02 -0600

    done?

--- DSA/Sarani.js
+++ DSA/Sarani.js
@@ -35,3 +35,50 @@ class BinaryTreeNode {
     //         this.right = node;
     //     }
     //  return this.data 
+
+    //Tree Traverse
+    //Planning- 
+    //For this challenge, write a function that takes a node and console.logs the data value each node and then the data for each of its children, but indented two spaces. The function should be general and not specific to this instance.
+    //do without indention first
+
+    const D = {
+        value: 'D',
+        children: []
+      };
+      
+      const B = {
+        value: 'B',
+        children: [D]
+      };
+      
+      const C = {
+        value: 'C',
+        children: []
+      };
+      
+      const F = {
+        value: 'F',
+        children: []
+      };
+      
+      const E = {
+        value: 'E',
+        children: [F]
+      };
+      
+      const A = {
+        value: 'A',
+        children: [B, C, E]
+      };
+
+      //does the node we are revaluating has a child? yes? print. no? traverse back up to root
+      function traverse(node) {
+        const root = 'A'
+
+        if(root === node.children){
+            console.log(this.left)
+
+        } else {
+
+        }
+    }
